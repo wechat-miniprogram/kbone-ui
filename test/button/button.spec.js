dbce8de4ee38abd35b4e9662ea893e55b6e8371a
wechat-miniprogram <villainthr@gmail.com>
2020-01-02 16:11:29 +0800

    update: add KButton testsuit

--- test/button/button.spec.js
+++ test/button/button.spec.js
@@ -1,5 +1,6 @@
 import { mount } from '@vue/test-utils'
 import KboneUI from 'kbone-ui'
+import sinon from 'sinon';
 
 const {
     KButton,KButtonArea,KView
@@ -31,4 +32,48 @@ test('render ok',()=>{
     expect(defaultTypeBtn.classes()).toEqual(['weui-btn','weui-btn_default'])
     expect(primaryTypeBtn.classes()).toEqual(['weui-btn','weui-btn_primary'])
     expect(warnTypeBtn.classes()).toEqual(['weui-btn','weui-btn_warn'])
+})
+
+test("render mini button",()=>{
+    const wrapper = mount(KButton,{
+        propsData: {
+            size: 'mini'
+          }
+    })
+    expect(wrapper.classes()).toEqual(['weui-btn','weui-btn_mini','weui-btn_default'])
+})
+
+test("render loading",()=>{
+    const wrapper = mount(KButton,{
+        propsData: {
+            loading: true
+          }
+    })
+
+    const loadingWrapper = wrapper.find('.weui-loading')
+    expect(loadingWrapper.is('i')).toBe(true)
+})
+
+test('trigger click when disabled',()=>{
+    const clickHandler = sinon.fake()
+
+    const parent = {
+        components:{
+            KButton,
+            KView
+        },
+        template:`
+            <KView>
+                <KButton id="btn" @click="clickHandler" :disabled="true" ></KButton>
+            </KView>
+        `,
+        methods:{
+            clickHandler
+        }
+    }
+
+    const btn = mount(parent, {}).find('#btn');
+    btn.trigger('click')
+    expect(clickHandler.called).toBe(false)
+
 })
